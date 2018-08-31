d3bd0383482b1b55491bb05e51b5eab7a7b95dbe
paulgaskell <paulgaskell1@gmail.com>
2018-08-31 07:33:19 +0100

    sdm

--- SDM.py
+++ SDM.py
@@ -1,178 +1,116 @@
 
 """
-Base class of the SDM algorithm
+SDM is forecasting method that provides the Bayesian optimal forecast of 
+the distance between x and y based on asumptions about the markov process 
+that drives lag structure change between t and t+1. 
 
-TODO
-    labels on graph axes
-    examples 
-    optimisations for Pr
-    OTCP recursion structure 
-    max a posteriori error 
-    linear regression comparison
-    ackage structure - pip installable 
-    
+See Gaskell, McGroarty, and Tiropanis 2015
 """
 
 import numpy as np
-import sys
-import logging 
-from pylab import show
-import matplotlib.pyplot as plt
-import seaborn as sns
-from functools import partial 
+from math import pi
+from pylab import show, figure
+import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger.info("package install is complete")
+logger.info("imports have completed")
 
-class Examples:
+def normal(x, mu, sig):
+    sig_2 = sig**2
+    Z = 1./np.sqrt(2*pi*sig_2)
+    d = (x-mu)**2
+    return Z*np.exp(-d/(2*sig_2))
+	
+
+class SDM:
     """
-    Making a variety of example t-series for testing/illustration 
-    purposes
+    class holding the SDM method
+    
+    example usage below
     """
     
-    def __init__(self):
-        self.x = None
-        self.y = None
-    
-    def make_y(self):
-        self.y = np.random.normal(0, 1, 100)
-        return self
-    
-    def make_x(self, lag):
-        self.x = np.append(
-                    self.y[lag:]+np.random.normal(0, 2, 100-lag), 
-                    np.random.normal(0, 1, lag)
-                    )
-        return self
-        
-    def out(self):
-        return self.x, self.y
-
-class Pr_surfaces:
-    
-    def create_pr_surface(self, lag, h):
-        """
-        Args: x, y, loss, lag, Pr
-        Returns: 
-            fills d with probabilities calculated as Pr(loss(x, y)), for 
-            each lag of x and y
-        """
-        self.lag = lag
-        self.h = h
-        self.D = np.zeros((len(self.x)-lag, 2, lag-1))
-        self.V = np.zeros(self.D.shape)
-		
-        loss = lambda x, y: (x-y)**2 
-        t = self.lag
-        end = len(self.x)
-        while t < end:
-            self.V[t-lag][0] = self.x[t-lag:t-1]
-            self.V[t-lag][1] = -self.x[t-lag:t-1]
-            self.D[t-lag] = loss(self.y[t], self.V[t-lag])
-            t += 1
-			
-        self.D = np.exp(-self.D/self.h)
-        return self
-
-class Recursions:
-
-    def recursive_bayes_simple(self):		
-		
-        self.W = np.zeros(self.D.shape)
-		
-        w = np.ones(self.D[0].shape)
-        w = w/np.sum(w)
-        for t, i in enumerate(self.D):
-            w = (w+0.00001)*i
-            w = w/np.sum(w)
-            self.W[t] = w
-			
-        return self
-
-class Results:
-
-    def results(self):
-        self.forecast = np.sum(np.sum(self.W*self.V, 1), 1)
-        print self.forecast
-        self.mae = np.mean(np.abs(self.forecast-self.y[self.lag:]))
-        return self
-       
-    def heatmap(self, surface='W'):
-        if surface == 'W':
-            s = self.W.copy()
-        elif surface == 'D':
-            s = self.D.copy()
-        elif surface == 'V':
-            s = self.V.copy()
-        else:
-            logger.error("you need to specifiy a plottable surface, W, D, V")
-
-        fig, axn = plt.subplots(2, 1, sharex=True, sharey=True)
-        cbar_ax = fig.add_axes([.91, .3, .03, .4])
-
-        for i, ax in enumerate(axn.flat):
-            sns.heatmap(
-                np.array([j[i] for j in s]).T, 
-                ax=ax,
-                cbar=i== 0,
-                vmin=0, 
-                vmax=1,
-                cbar_ax=None if i else cbar_ax
-                )
-
-        #fig.tight_layout(rect=[0, 0, .9, 1])
-        show()
-        
-
-class SDM(Pr_surfaces, Recursions, Results):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    
+        logger.info("t-series dimensions = {}, {}".format(
+                        str(x.shape), str(y.shape)
+                        ))
+    
+    def create_d_surface(self, lags):
+        """
+        """
+        D = np.zeros((len(self.x)-lags, 2, lags))
+        logger.info("D dim = {}".format(D.shape))
+        for t in range(lags, len(self.x), 1):
+            D[t-lags][0] = x[t-lags:t][::-1]-y[t]
+            D[t-lags][1] = y[t-lags:t][::-1]-x[t]
 
-        try:
-            assert len(self.x) == len(self.y)
-        except AssertionError as E:
-            logger.error("time series must be of equal length")
-            sys.exit()
-	
-        logger.info("tseries loaded lengths {}".format(str(x.shape)))
+        self.lags = lags
+        self.D = D 
+        return self    
+    
+    def sdm(self, top_paths=False):
+        """
+        """
+        mu = np.mean(self.D)
+        sig = np.std(self.D)
+        logger.info("mu={} sig={}".format(mu, sig))
 		
-class Optimiser:
-    def __init__(self, sdm):
-        self.sdm = sdm
+        W = np.ones(self.D.shape)/(self.lags*2)
+        logger.info("W dim = {}".format(W.shape))
+        for t in range(1, len(self.D), 1): 
+            if top_paths:
+                w = W[t-1]
+                for i in range(len(self.D[t-1])):
+                    w[i] = W[t-1][i]
+                    w[i] = w[i]+np.append(0, W[t-1][i][:-1])
+                    w[i] = w[i]+np.append(W[t-1][i][1:], 0)
+                w = w/3
+            else:
+                w = W[t-1]
+            
+            W[t] = (w+0.00001)*normal(self.D[t], mu, sig)
+            W[t] = W[t]/np.sum(W[t])
+            print W[t].shape, np.sum(W[t])
+            
+        self.W = W
+        return self 
 
-    def evo_optimiser(self):
-        h = np.random.random()
-        err = 1
-        t = 0
-        while t < 100:
-            a = np.random.normal(0, err)
-            self.sdm = self.sdm.create_pr_surface(10, h+a).recursive_bayes_simple(
-                    ).results()
-            
-            err_ = self.sdm.mae
-            
-            if err_ < err:
-                h = h+a
-                err = err_
-            
-            print err, h, a, t
-            t += 1
-            
-        return self
-			
+        
 if __name__ == "__main__":
 
-    x, y = Examples().make_y().make_x(5).out()    
-    sdm = SDM(x, y)
-    
-    # np.exp(-((x-y)**2)/h)
-    # np.exp(-(((a+bx)-y)**2)/h)
-    
-    op = Optimiser(sdm)
-    op = op.evo_optimiser()
+    # EXAMPLE USAGE
 
-    op.sdm.heatmap()
+    # make an example time series
+    x = np.random.normal(0, 1, 200)
+    y = np.random.normal(0, .1, 200)
+    for n, i in enumerate(x):
+        if n < 20:
+            y[n] += x[n+1]
+        elif n < 70:
+            y[n] += x[n-10]
+        elif n < 120:
+            y[n] += x[n-30]
+        elif n < 150:
+            y[n] += x[n-35]
+        else:
+            y[n] += x[n]
+    
+    y = (y-np.mean(y))/np.std(y)
+    
+    x = np.append(x, x)
+    y = np.append(y, y)
+    
+    # run the TOP method
+    sdm = SDM(x, y)
+    sdm = sdm.create_d_surface(40).sdm(top_paths=True)    
+    fig = figure()
+    ax = fig.add_subplot(211)
+    ax1 = fig.add_subplot(212)
+    ax.imshow([i[0] for i in sdm.W.T], aspect='auto')
+    ax1.imshow([i[1] for i in sdm.W.T], aspect='auto')
+    show()
 	
